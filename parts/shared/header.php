e25615f38c614e7f855c12fb616623f893b78ac2
Dave Allen <davidallen401@btinternet.com>
2013-01-20 20:21:58 +0000

    Added logo

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,5 +1,6 @@
 
 <header>
-	<h1><a href="#"><?php bloginfo( 'name' ); ?></a></h1>
+	<a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo bloginfo('stylesheet_directory'); ?>/images/logo.png" alt="logo" /> </a>
+	<h1 class="accessibility"><a href="#"><?php bloginfo( 'name' ); ?></a></h1>
 	<?php wp_nav_menu( ); ?>
 </header>
