3f1462205792e7c4ff2f4161421837d591a07ab1
Dave Allen <davidallen401@btinternet.com>
2013-01-21 20:42:03 +0000

    Start a branch for media query adjusts

--- page-home.php
+++ page-home.php
@@ -12,7 +12,7 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<h2><?php the_title(); ?></h2>
+
 <?php the_content(); ?>
 
 <?php endwhile; ?>
