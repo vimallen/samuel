03a47e2de7dfb908fb390718ba3e002bb0590ce1
Dave Allen <davidallen401@btinternet.com>
2013-01-26 19:43:14 +0000

    Sat start

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -1,4 +1,5 @@
 	
 	<footer>
-		&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.
+		p>&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
+<p><a href="http://www.daveallengraphics.com/" title="Graphic design for print and web" class="credit">Website design by Dave Allen Graphics</a></p>
 	</footer>
