791072f0a6cd821acc8d173e04e55902c5a50016
Dave Allen <davidallen401@btinternet.com>
2013-02-03 19:38:39 +0000

    Sun start

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,6 +1,6 @@
 
 <header>
-	<a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo bloginfo('stylesheet_directory'); ?>/images/logo.png" alt="logo" /> </a>
+	<a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo bloginfo('stylesheet_directory'); ?>/images/logo.png" alt="home page" /> </a>
 	<h1 class="accessibility"><a href="#"><?php bloginfo( 'name' ); ?></a></h1>
 	
 	<?php wp_nav_menu( ); ?>
