6b235595e94d4b2e9e2bee7c0d776a687a176f26
Dave Allen <davidallen401@btinternet.com>
2013-02-10 20:55:44 +0000

    Added meteor

--- page-home.php
+++ page-home.php
@@ -10,7 +10,7 @@
 
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
-
+<?php if ( function_exists( 'meteor_slideshow' ) ) { meteor_slideshow(); } ?>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 <?php the_content(); ?>
