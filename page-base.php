b4d717dcc981d1049cb8ee46d6b40b27e94c1dfa
Dave Allen <davidallen401@btinternet.com>
2013-02-10 18:56:49 +0000

    Sun start

--- page-base.php
+++ page-base.php
@@ -10,9 +10,10 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<div class="main intro">
 <h2><?php the_title(); ?></h2>
 <?php the_content(); ?>
 
 <?php endwhile; ?>
-
+</div><!--main-->
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
