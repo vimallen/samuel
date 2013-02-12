7986d1b9477e6a9474c379cc2db1b0065d9c29e0
Dave Allen <davidallen401@btinternet.com>
2013-02-12 22:44:14 +0000

    Added images to home

--- page-home.php
+++ page-home.php
@@ -12,8 +12,9 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 <?php if ( function_exists( 'meteor_slideshow' ) ) { meteor_slideshow(); } ?>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
+<div class="main-home">
 <?php the_content(); ?>
+</div><!--main-home-->
 
 <?php endwhile; ?>
 
