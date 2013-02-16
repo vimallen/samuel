863d2e4f41381aa441b8aa60f9d4073a65738d0c
Dave Allen <davidallen401@btinternet.com>
2013-02-16 22:05:49 +0000

    Used min-height

--- page-contacts.php
+++ page-contacts.php
@@ -16,7 +16,7 @@
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<div class="main contacts">
+<div class="main">
 <h2><?php the_title(); ?></h2>
 <?php
           $loop = new WP_Query(array('post_type' => 'contacts',
@@ -31,7 +31,8 @@
           $screenshot_url = $custom["screenshot_url"][0];
           $website_url = $custom["website_url"][0];
      ?>
-          <div class="contact g  one-third  lap-one-half  palm-one-whole">
+             
+          <div class="contact-single g one-third lap-one-half palm-one-whole">
           <h4 class="contactHeader"><?php the_title(); ?></h4>
           <a href="<?=$website_url?>"><?php the_post_thumbnail(); ?> </a>
           <?php the_content(); ?>
