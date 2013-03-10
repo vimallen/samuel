a71d5421cfcde89924e104599d3a8d146062dcb1
Dave Allen <davidallen401@btinternet.com>
2013-03-10 19:59:15 +0000

    Next: links on collection page

--- functions.php
+++ functions.php
@@ -10,29 +10,29 @@
 	 */
 
 	/* ========================================================================================================================
-	
+
 	Required external files
-	
+
 	======================================================================================================================== */
 
 	require_once( 'external/starkers-utilities.php' );
 
 	/* ========================================================================================================================
-	
+
 	Theme specific settings
 
 	Uncomment register_nav_menus to enable a single menu with the title of "Primary Navigation" in your theme
-	
+
 	======================================================================================================================== */
 
 	add_theme_support('post-thumbnails');
-	
+
 	register_nav_menus(array('primary' => 'Primary Navigation'));
 
 	/* ========================================================================================================================
-	
+
 	Actions and Filters
-	
+
 	======================================================================================================================== */
 
 	add_action( 'wp_enqueue_scripts', 'starkers_script_enqueuer' );
@@ -54,11 +54,11 @@
  }
 
 	/* ========================================================================================================================
-	
+
 	Custom Post Types - include custom post types and taxonimies here e.g.
 
 	e.g. require_once( 'custom-post-types/your-custom-post-type.php' );
-	
+
 	======================================================================================================================== */
 
 	// Creates contact post type
@@ -83,7 +83,7 @@
 		'page-attributes',)
 		) );
 
-// Creates contact post type
+// Creates module-home type
 		register_post_type('module-home', array(
 		'label' => 'Module-home',
 		'public' => true,
@@ -104,10 +104,35 @@
 		'author',
 		'page-attributes',)
 		) );
+
+
+// Creates module-collection type
+		register_post_type('module-collection', array(
+		'label' => 'Module-collection',
+		'public' => true,
+		'show_ui' => true,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'rewrite' => array('slug' => 'recipes'),
+		'query_var' => true,
+		'supports' => array(
+		'title',
+		'editor',
+		'excerpt',
+		'trackbacks',
+		'custom-fields',
+		'comments',
+		'revisions',
+		'thumbnail',
+		'author',
+		'page-attributes',)
+		) );
+
+
 	/* ========================================================================================================================
-	
+
 	Scripts
-	
+
 	======================================================================================================================== */
 
 	/**
@@ -125,7 +150,7 @@
         wp_enqueue_style( 'screen' );
 
         /**
-        Adding a parameter of false puts the script in the head, where Modernizr needs to be, for example. 
+        Adding a parameter of false puts the script in the head, where Modernizr needs to be, for example.
         Adding true puts the script before the closing tag.
         **/
 
@@ -135,7 +160,7 @@
 		wp_register_script( 'respond', get_template_directory_uri().'/js/respond.min.js', array(), '1.1.0', true );
 		wp_enqueue_script( 'respond' );
 
-	}	
+	}
 
 //	Customising the dashboard
 
@@ -162,21 +187,21 @@ function my_login_logo_url_title() {
 add_filter( 'login_headertitle', 'my_login_logo_url_title' );
 
 	/* ========================================================================================================================
-	
+
 	Comments
-	
+
 	======================================================================================================================== */
 
 	/**
-	 * Custom callback for outputting comments 
+	 * Custom callback for outputting comments
 	 *
 	 * @return void
 	 * @author Keir Whitaker
 	 */
 	function starkers_comment( $comment, $args, $depth ) {
-		$GLOBALS['comment'] = $comment; 
+		$GLOBALS['comment'] = $comment;
 		?>
-		<?php if ( $comment->comment_approved == '1' ): ?>	
+		<?php if ( $comment->comment_approved == '1' ): ?>
 		<li>
 			<article id="comment-<?php comment_ID() ?>">
 				<?php echo get_avatar( $comment ); ?>
--- page-home.php
+++ page-home.php
@@ -11,8 +11,8 @@
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 <?php if ( function_exists( 'meteor_slideshow' ) ) { meteor_slideshow(); } ?>
-<div class="main-home">
-  <?php the_content(); ?> 
+<div class="main">
+  <?php the_content(); ?>
 <?php
           $loop = new WP_Query(array('post_type' => 'module-home',
           'posts_per_page' => 10,
@@ -21,15 +21,15 @@
           ));
      ?>
      <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-     <?php    
+     <?php
           $custom = get_post_custom($post->ID);
           $screenshot_url = $custom["screenshot_url"][0];
           $website_url = $custom["website_url"][0];
      ?>
-     <div class="module-home g one-quarter lap-one-half palm-one-whole">
+     <div class="module g one-quarter lap-one-half palm-one-whole">
           <h3 class="underline-dashed"><?php the_title(); ?></h3>
           <?php the_post_thumbnail(); ?> </a>
-  <?php the_content(); ?>        
+  <?php the_content(); ?>
 </div><!-- module -->
 
 
