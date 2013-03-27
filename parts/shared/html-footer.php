d96968b4a57f8f93f40d8b20bfd3ee5347e90a66
Dave Allen <davidallen401@btinternet.com>
2013-03-27 21:11:02 +0000

    Footer tweak

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -2,7 +2,7 @@
 	<?php wp_footer(); ?>
 	<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            var _gaq=[['_setAccount','UA-1885475-11'],['_trackPageview']];
             (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
             g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
             s.parentNode.insertBefore(g,s)}(document,'script'));
