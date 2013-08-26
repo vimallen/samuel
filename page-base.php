4d32daf8040c95ab04f431ec1bd0b9a0ca243e3c
Dave Allen <davidallen401@btinternet.com>
2013-08-26 22:29:34 +0100

    Tweaked intro padding right

--- page-base.php
+++ page-base.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *Template name: page-base 
+ *Template name: page-base
  *
  * @package 	WordPress
  * @subpackage 	Starkers
@@ -10,7 +10,7 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<div class="main intro">
+<div class="main intro-page-base">
 <h2><?php the_title(); ?></h2>
 <?php the_content(); ?>
 
