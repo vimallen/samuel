8377d668fb964c180f6dd8a0caa07caa20e13407
Dave Allen <davidallen401@btinternet.com>
2014-09-11 22:14:10 +0100

    Pre-live

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -1,5 +1,13 @@
 	
 	<footer>
+
+<p><a href="https://www.facebook.com/samueltweedfabrics?fref=ts"class="social">Facebook</a></p>
+
+<p><a href="https://twitter.com/samtweedfabrics" class="social">Twitter</a></p>
+
+<p><a href="http://instagram.com/samueltweedfabrics" class="social">Instagram</a></p>
+
+
 		<p>&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
 <p>Website design by <a href="http://www.daveallengraphics.com/" title="Graphic design for print and web" 
 class="credit">Dave Allen Graphics</a></p>
