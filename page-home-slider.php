3f16e567d1e3ea6fbf123024419f55e4b7ac25d3
Dave Allen <davidallen401@btinternet.com>
2015-01-24 22:56:21 +0000

    Meteor slides installed

---
+++ page-home-slider.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *Template name: page-home-slider
+ *
+ * @package 	WordPress
+ * @subpackage 	Starkers
+ * @since 		Starkers 4.0
+ */
+?>
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+<?php if ( function_exists( 'meteor_slideshow' ) ) { meteor_slideshow(); } ?>
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<div class="grid grid-pad main">
+<!--<h2><?php the_title(); ?></h2>-->
+
+<?php the_content(); ?>
+
+<?php endwhile; ?>
+</div><!--grid-->
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
